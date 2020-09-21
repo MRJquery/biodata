201ec465a52d59d3caf25b75231107d63e371e64
MRJquery <azuraxz12@gmail.com>
2020-09-21 10:47:41 +0700

    initial commit

---
+++ build/precache-manifest.d06a57c3a2f8cf7a05148e3edca75d2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654908df96bc7f3f8bb2977427204a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeff3ac8d1451b5617db",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "3fa0e4b719dac87cff46",
+    "url": "/static/css/main.0a2674ce.chunk.css"
+  },
+  {
+    "revision": "aeff3ac8d1451b5617db",
+    "url": "/static/js/2.16a43d0b.chunk.js"
+  },
+  {
+    "revision": "3fa0e4b719dac87cff46",
+    "url": "/static/js/main.448d6eae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dc964e942ba1bcba237614f898295aa3",
+    "url": "/static/media/saya.dc964e94.jpg"
+  }
+]);
